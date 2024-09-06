366cacfa2dd67be198a130c41bfb485f4a6fa459
NewBieMaster123 <jaeselwaywaya674@gmail.com>
2024-09-06 12:46:08 +1200

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,14 @@
+/* Anna
+// September 6,20204
+First Programming Session/*
+
+*/
+public class Test {
+    public static void main(String[] args) {
+        
+        System.out.print("Anna \t\tRhodora \nQuitaleg");
+        //JavaEscape characters
+        System.out.println("\n\n\nAnna\"s Class");
+        System.out.print("I love Java programming");// Anna
+    }
+}
